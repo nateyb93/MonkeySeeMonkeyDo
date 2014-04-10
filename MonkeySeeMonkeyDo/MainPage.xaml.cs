d6863b82c8ede9189e8381dd26e664d790ef5f27
nateyb93 <brownn15@up.edu>
2014-04-10 11:30:35 -0700

    cloud hookups doe

--- MonkeySeeMonkeyDo/MainPage.xaml.cs
+++ MonkeySeeMonkeyDo/MainPage.xaml.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -37,7 +41,56 @@ namespace MonkeySeeMonkeyDo
 
         private void Activity_Tapped(object sender, TappedRoutedEventArgs e)
         {
+            Details.Items.Clear();
+            List<Activity> activities = Activity.GetList();
 
+            foreach(Activity a in activities)
+            {
+                //add new ListViewItem with listItem style and text matching the string applied in activity class.
+                ListViewItem item = new ListViewItem
+                {
+                    Style = (Style)Application.Current.Resources["listItem"],
+                    Content = new TextBlock
+                    {
+                        Style = (Style)Application.Current.Resources["listItemText"],
+                        Text = a.ActivityName
+                    }                                       
+                };
+                
+                Details.Items.Add(item);
+            }
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //create new test user
+            User user = new User("nateyb93");
+
+            //add user to database on parallel thread
+            IAsyncAction asyncAction = Windows.System.Threading.ThreadPool.RunAsync(
+                (workItem) =>
+                {
+                    AddItem(user);
+                }
+            );
+        }
+
+        /// <summary>
+        /// Adds a user to online database
+        /// </summary>
+        /// <param name="u">User to add to database</param>
+        private async void AddItem(User u)
+        {
+            try
+            {
+                await App.MobileService.GetTable<User>().InsertAsync(u);
+            }
+            catch (MobileServiceInvalidOperationException e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+        }
+
+
     }
 }
